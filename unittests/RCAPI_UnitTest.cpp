47349e758157cca3763d9b240d578b9f45f0470e
179sd <annonproxy@protonmail.com>
2018-08-07 01:18:09 -0400

    Login testcase added

--- unittests/RCAPI_UnitTest.cpp
+++ unittests/RCAPI_UnitTest.cpp
@@ -1,2 +1,15 @@
+#define CATCH_CONFIG_MAIN
 #include "catch.hpp"
 #include "../include/RCAPI.h"
+#include <iostream>
+
+REQUIRE( "The login function works correctly and catches errors" ){
+	RCAPI h("https://chat.systemspace.link/api/v1/");
+	std::string password;
+	std::cin >> password;
+	REQUIRE( h.login("179", password) == RCAPI_GOOD);
+	REQUIRE( h.login("", password) == RCAPI_EMPTYSTRING);
+	REQUIRE( h.login("179", "") == RCAPI_EMPTYSTRING);
+	REQUIRE( h.login("179", "wrongpassword") == RCAPI_INCORRECTLOGIN);
+	REQUIRE( h.login("wrongusername", password) == RCAPI_INCORRECTLOGIN);
+}
